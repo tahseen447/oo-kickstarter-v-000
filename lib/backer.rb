c97f24f8748cbada9138115b6dbeec3e2a215292
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-11 04:44:51 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,8 +12,8 @@ class Backer
   end
 
   def back_project(project)
-    @backed_projects = project
-    project.backers << project
+    @backed_projects << project
+    project.backers << self
   end
 
 end
