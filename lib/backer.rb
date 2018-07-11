d45291603af788aabb45b91cf6ad96b74c9b63e1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-11 04:48:07 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+  attr_accessor :backed_projects, :name
+  @@backers = []
+
+  def initialize(name)
+    @name = name
+    @@backers << self
+    @backed_projects = []
+  end
+
+  def self.backers
+    @@backers
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  attr_accessor :backers, :title
+  @@projects = []
+
+  def initialize(name)
+    @title = name
+    @backers = []
+    @@projects << self
+  end
+
+  def self.projects
+    @@projects
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
